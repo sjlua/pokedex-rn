4d0544fe10426a031ec187a52f953f293d20faa2
Sean Lua <seanjlua10@gmail.com>
2025-11-03 23:48:44 +1100

    Added a tab bar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,34 @@
-import { Stack } from "expo-router";
-
-// handle navigation and animations
+// Handle navigation and animations
 // options changes how the page appears
+
+import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import { Tabs } from "expo-router";
+
+// A stable tab bar layout.
 export default function RootLayout() {
-  return <Stack>
-    <Stack.Screen name="index" options={{ title: "Pokedex" }}></Stack.Screen>
-    <Stack.Screen name="statistics" options={{ title: "Pokemon Stats", headerBackButtonDisplayMode: "minimal" }}></Stack.Screen>
-  </Stack>;
+  return <Tabs>
+    <Tabs.Screen name="index" options={{ title: "Pokédex", tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="pokeball" size={size} color={color} />) }}></Tabs.Screen>
+    <Tabs.Screen name="statistics" options={{ title: "Pokémon Stats", href: null, headerBackButtonDisplayMode: "minimal" }}></Tabs.Screen>
+    <Tabs.Screen name="favouritePokemon" options={{ title: "Favourite Pokémon", tabBarIcon: ({color, size}) => (<MaterialIcons name="favorite-border" size={size} color={color} />), headerBackButtonDisplayMode: "minimal" }}></Tabs.Screen>
+  </Tabs>;
 }
+// href null allows pages to be hidden from the tab bar
+
+
+// A NativeTabs implementation of the bottom bar
+// Not implemented as it breaks page navigation
+// export default function RootLayout() {
+//   return (
+//     <NativeTabs>
+//       <NativeTabs.Trigger name="index">
+//         <Label>Pokedex</Label>
+//         <Icon sf="book.fill" drawable="ic_library_books" />
+//       </NativeTabs.Trigger>
+
+//       <NativeTabs.Trigger name="favouritePokemon">
+//         <Icon sf="heart.fill" drawable="ic_favorite" />
+//         <Label>Favourites</Label>
+//       </NativeTabs.Trigger>
+//     </NativeTabs>
+//   );
+// }
