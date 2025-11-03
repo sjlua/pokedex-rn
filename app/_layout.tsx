a24c3e3f67f829a6cfe3018855f04f6f4f9f4700
Sean Lua <seanjlua10@gmail.com>
2025-11-04 00:34:11 +1100

    A new search tab

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -8,6 +8,7 @@ import { Tabs } from "expo-router";
 export default function RootLayout() {
   return <Tabs>
     <Tabs.Screen name="index" options={{ title: "Pokédex", tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="pokeball" size={size} color={color} />) }}></Tabs.Screen>
+    <Tabs.Screen name="search" options={{ title: "Search", tabBarIcon: ({color, size}) => (<MaterialIcons name="search" size={size} color={color} />) }}></Tabs.Screen>
     <Tabs.Screen name="statistics" options={{ title: "Pokémon Stats", href: null, headerBackButtonDisplayMode: "minimal" }}></Tabs.Screen>
     <Tabs.Screen name="favouritePokemon" options={{ title: "Favourite Pokémon", tabBarIcon: ({color, size}) => (<MaterialIcons name="favorite-border" size={size} color={color} />), headerBackButtonDisplayMode: "minimal" }}></Tabs.Screen>
   </Tabs>;
