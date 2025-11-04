4cb1aa262af0f5ef50171f4f16718cedc6ba0e3a
Sean Lua <seanjlua10@gmail.com>
2025-11-04 14:26:16 +1100

    Bottom bar name changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,7 +10,8 @@ export default function RootLayout() {
     <Tabs.Screen name="index" options={{ title: "Pokédex", tabBarIcon: ({color, size}) => (<MaterialCommunityIcons name="pokeball" size={size} color={color} />) }}></Tabs.Screen>
     <Tabs.Screen name="search" options={{ title: "Search", tabBarIcon: ({color, size}) => (<MaterialIcons name="search" size={size} color={color} />) }}></Tabs.Screen>
     <Tabs.Screen name="statistics" options={{ title: "Pokémon Stats", href: null, headerBackButtonDisplayMode: "minimal" }}></Tabs.Screen>
-    <Tabs.Screen name="favouritePokemon" options={{ title: "Favourite Pokémon", tabBarIcon: ({color, size}) => (<MaterialIcons name="favorite-border" size={size} color={color} />), headerBackButtonDisplayMode: "minimal" }}></Tabs.Screen>
+    <Tabs.Screen name="partnerPokemon" options={{ title: "Poké Partner", tabBarIcon: ({color, size}) => (<MaterialIcons name="favorite-border" size={size} color={color} />), headerBackButtonDisplayMode: "minimal" }}></Tabs.Screen>
+    <Tabs.Screen name="info" options={{ title: "App Info", tabBarIcon: ({color, size}) => (<MaterialIcons name="info-outline" size={size} color={color} />)}}></Tabs.Screen>
   </Tabs>;
 }
 // href null allows pages to be hidden from the tab bar
