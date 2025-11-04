5d3ab9054f24e767efd0eee31a3effd4b9e2b0b9
Sean Lua <seanjlua10@gmail.com>
2025-11-04 13:18:11 +1100

    Basic search UI

---
+++ app/search.tsx
@@ -0,0 +1,91 @@
+import { useEffect, useState } from "react";
+import { Button, ScrollView, StyleSheet, Text, TextInput, View } from "react-native";
+
+interface Pokemon {
+  pokedex: number;
+  name: string;
+  imageFrontLink: string;
+  imageBackLink: string;
+  imageFrontShinyLink: string;
+}
+
+export default function Search() {
+    const [query, setQuery] = useState<string>(""); // user input field
+    const [selectedName, setSelectedName] = useState<string | null>(null); // becomes non-null after submitting an answer
+
+    useEffect(() => { searchPokemon() }, [selectedName])
+
+    async function searchPokemon() {
+        try {
+            if (selectedName) {
+                // fetch takes URL and receives a response
+                const unformattedResponse = await fetch("https://pokeapi.co/api/v2/pokemon/" + selectedName);
+                // format to json
+                const jsonData = await unformattedResponse.json();
+
+                const mapToFavourite: Pokemon = {
+                    pokedex: jsonData.id,
+                    name: jsonData.name,
+                    imageFrontLink: jsonData.sprites.front_default,
+                    imageFrontShinyLink: jsonData.sprites.front_shiny,
+                }
+
+                // update favourite pokemon stats
+                // setFavouriteMon(mapToFavourite)
+            }
+        }
+        catch (e) {
+            console.log(e)
+        }
+    }
+
+    return (
+        <ScrollView 
+        contentContainerStyle={{ gap: 10, padding: 20 }}>
+            <View style={styles.container}>
+                <Text style={styles.question}>"Search for a Pokémon"</Text>
+                <TextInput 
+                    placeholder="Name or national pokedex number (e.g. pikachu or 25)"
+                    value={query}
+                    onChangeText={setQuery}
+                    autoCapitalize="none"/>
+                <Button
+                    key={"search"} 
+                    title="Search"
+                    accessibilityLabel="Search for a Pokémon." 
+                    onPress={() => {
+                        if (!query.trim()) return;
+                        setSelectedName(query.trim());
+                        setQuery("")
+                    }}/>
+            </View>
+        </ScrollView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        alignItems: "center", 
+        width: "100%",
+        gap: 8
+    },
+
+    question: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+
+    smallQuestion: {
+        fontSize: 15,
+        fontWeight: 'bold'
+    },
+
+    smallText: {
+        fontSize: 15,
+    },
+
+    name: {
+        fontSize: 40,
+        fontWeight: 'bold'
+    }
+})
