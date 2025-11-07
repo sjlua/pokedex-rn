205960a1942a8a064070bdd48c99c28e3295918f
Sean Lua <seanjlua10@gmail.com>
2025-11-08 00:23:45 +1100

    Added expo-blur for blurry UI elements

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,27 @@
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
+import { BlurView } from "expo-blur";
 import { Tabs } from "expo-router";
+import { StyleSheet } from "react-native";
 
 export default function TabsLayout() {
   return (
-    <Tabs>
+    <Tabs screenOptions={{ 
+      tabBarActiveTintColor: "#b60c0cff", 
+      tabBarStyle: { position: 'absolute' }, 
+
+      tabBarBackground: () => (
+        // see https://docs.expo.dev/versions/latest/sdk/blur-view/#experimentalblurmethod
+        // on why experimentalBlurMethod="dimezisBlurView" is used
+        <BlurView tint="prominent" intensity={80} style={StyleSheet.absoluteFill} experimentalBlurMethod="dimezisBlurView" />
+      ),
+      headerBackground: () => (
+          <BlurView tint="prominent" intensity={80} style={StyleSheet.absoluteFill} experimentalBlurMethod="dimezisBlurView" />
+        )
+      }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: "Pokedex",
+          title: "Pokédex RN",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="pokeball" size={size} color={color} />
           )
@@ -27,7 +41,7 @@ export default function TabsLayout() {
       <Tabs.Screen
         name="partnerPokemon"
         options={{
-          title: "Partner",
+          title: "My Partner",
           tabBarIcon: ({ color, size }) => (
             <MaterialIcons name="favorite-border" size={size} color={color} />
           )
@@ -37,7 +51,7 @@ export default function TabsLayout() {
       <Tabs.Screen
         name="info"
         options={{
-          title: "Info",
+          title: "App Info",
           tabBarIcon: ({ color, size }) => (
             <MaterialIcons name="info-outline" size={size} color={color} />
           )
