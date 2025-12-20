08dcbbacff40f0e6ea75ff2498818695a191541f
Sean Lua <seanjlua10@gmail.com>
2025-12-21 00:31:12 +1100

    Added theming and replaced the old index with the old collect page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,10 @@
-import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { Picker } from '@react-native-picker/picker';
+import { Text } from "@react-navigation/elements";
 import { Link } from "expo-router";
 import { useEffect, useState } from "react";
-import { Button, Image, ScrollView, StyleSheet, Text, View } from "react-native";
-
-// Base version from Code with Beto: https://youtu.be/BUXnASp_WyQ
+import { Button, FlatList, Image, StyleSheet, useColorScheme, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
+import { Colours } from "../../constants/colours";
 
 interface Pokemon {
   name: string;
@@ -40,165 +41,216 @@ const bgColourByType: Record<string, string> = {
   fairy: "#F4B6D9"
 }
 
-export default function Index() {
-  const bottomBarTabHeight = useBottomTabBarHeight();
+export default function Collection() {
+    // view region picker button
+    const [viewRegionPicker, setRegionPicker] = useState<boolean>(false);
+    // region picker
+    const [selectedRegion, setRegion] = useState<number>(0);
+    
+    // a list of Pokemon objects and the setter
+    const [listPokemon, setPokemonData] = useState<Pokemon[]>([]);
 
-  // a list of Pokemon objects and the setter?
-  const [listPokemon, setPokemonData] = useState<Pokemon[]>([]);
+    // tracks the number of items on screen from 0-currentPage.
+    const [currentPage, setCurrentPage] = useState<number>(0)
 
-  // tracks the number of items on screen from 0-currentPage.
-  const [currentPage, setCurrentPage] = useState<number>(1)
+    // Get the colour scheme from app.json userInterfaceStyle, null fallback is light
+    const colourScheme = useColorScheme() ?? 'light'
+    const theme = Colours[colourScheme]
 
-  // useEffect first param runs it on first boot
-  useEffect(() => {
-    // Fetch Pokemon, create UI
-    // The list that currentPage is in, ensures that useEffect is called 
-    // whenever changes are made to whatever state variables are in the Depedency List
-  fetchPokemon()
-  }, [currentPage])
+    // useEffect first param runs it on first boot
+    useEffect(() => {
+        // Fetch Pokemon, create UI
+        // The Depedency List that currentPage is in below, ensures that useEffect is called 
+        // whenever changes are made to whatever state variables are in the Depedency List
+    fetchPokemon()
+    }, [currentPage, selectedRegion])
 
-  async function fetchPokemon() {
-    try {
-      // fetch takes URL and receives a response
-      const unformattedResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${currentPage}`);
+    async function fetchPokemon() {
+        try {
+        // fetch takes URL and receives a response
+        // This URL, starting from the offset region, gets the first currentPage pokemon
+        const unformattedResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/?limit=${currentPage}&offset=${selectedRegion}`);
 
-      // format to json
-      const jsonData = await unformattedResponse.json();
-      
-      // output jsonData to terminal to understand what the api responds with
-      // console.log(jsonData[0]);
+        // format to json
+        const jsonData = await unformattedResponse.json();
+        
+        // output jsonData to terminal to understand what the api responds with
+        // console.log(jsonData[0]);
 
-      // get detailed info for each pokemon in parallel
-      const getPokemonStats = await Promise.all(
-        jsonData.results.map(async (pokemon: any) => {
-          const response = await fetch(pokemon.url);
-          const stats = await response.json();
-          return {
-            name: pokemon.name,
-            imageFrontLink: stats.sprites.front_default, // main front sprite
-            imageBackLink: stats.sprites.back_default, // main front sprite
-            types: stats.types
-          }
-        })
-      )
+        // get detailed info for each pokemon in parallel
+        const getPokemonStats = await Promise.all(
+            jsonData.results.map(async (pokemon: any) => {
+            const response = await fetch(pokemon.url);
+            const stats = await response.json();
+            return {
+                name: pokemon.name,
+                imageFrontLink: stats.sprites.front_default, // main front sprite
+                imageBackLink: stats.sprites.back_default, // main front sprite
+                types: stats.types
+            }
+            })
+        )
 
-      // update listPokemon data based on the results array located within the API json data
-      // setPokemonData(jsonData.results)
+        // update listPokemon data based on the results array located within the API json data
+        // setPokemonData(jsonData.results)
 
-      // update listPokemon data based on the results from further fetch
-      setPokemonData(getPokemonStats)
-      // console.log(JSON.stringify(getPokemonStats[0], null, 2))
+        // update listPokemon data based on the results from further fetch
+        setPokemonData(getPokemonStats)
+        // console.log(JSON.stringify(getPokemonStats[0], null, 2))
 
-      // if fail, print err to console
-    } catch(e) {
-      console.log(e)
+        // if fail, print err to console
+        } catch(e) {
+        console.log(e)
+        }
+
+    } 
+
+    // When called, add 2 more pages worth of Pokemon
+    const loadMorePokemon = async () => {
+        setCurrentPage(currentPage + 18)
+    };
+
+    const showRegionPicker = async () => {
+        viewRegionPicker ? setRegionPicker(false) : setRegionPicker(true)
     }
-  }
-  return (
-    // replaces standard unscrollable view with scrollable
-    <ScrollView
-    contentContainerStyle={{ gap: 10, padding: 10, paddingBottom: 10 + bottomBarTabHeight }}>
-      {listPokemon.map((pokemon: Pokemon) => (
-        <Link
-        key={pokemon.name}
-        href={{ pathname: "/statistics", params: {name: pokemon.name}}}
-        style={[styles.cardLayout, 
-            {
-            // + 70 makes the opacity 70%
-            // ignore type warning
-            // @ts-ignore
-            backgroundColor: bgColourByType[pokemon.types[0].type.name] + 70,
-          }]}>
-           {/* for each pokemon, create a View with the key of pokemon.name, containing it's name ... */}
-          <View style={styles.cardContent}>
+    
+    return (
+        // replaces standard unscrollable view with scrollable
+        <SafeAreaProvider>
+            <SafeAreaView>
+                <View style={[styles.questionRow, {backgroundColor: theme.background}]}>
+                    <Text style={[styles.question, {color: theme.text}]}>{"Fly to another region?"}</Text>
+                    <Button title={viewRegionPicker ? "Close" : "Search"} color={theme.iconColorFocused} onPress={() => showRegionPicker()}/>
+                </View>
 
-            {/* Name */}
-            <Text key={pokemon.name} style={styles.name}>
-              {pokemon.name.toUpperCase()}
-            </Text>
-
-            {/* Types */}
-            <View style={styles.typesRow}>
-              {pokemon.types.map((type) => (
-              <Text key={pokemon.name + type.type.name} style={styles.type}>{type.type.name}</Text>
-            ))}
-            </View>
-
-            {/* Sprites */}
-            <View style={styles.imagesRow}>
-              <Image
-              source={{uri: pokemon.imageFrontLink}}
-              style={{ width: 150, height: 150 }} />
-              <Image
-              source={{uri: pokemon.imageBackLink}}
-              style={{ width: 150, height: 150 }} />
-            </View>
-          </View>
-        </Link>
-      ))
-      }
-      <View style={styles.buttonRow}>
-        {/* Don't allow the user to request for less pokemon than 10, since 10 is the page increment */}
-        {currentPage < 10 ? null : <Button 
-        title="View Less"
-        color={"#b60c0cff"}
-        onPress={() => {
-          const updatePage: number = currentPage >= 11 ? currentPage - 10 : currentPage
-          // Update current page number
-          setCurrentPage(updatePage)
-        }}/>}
-
-        {/* extra view needed to allow right aligned More text */}
-        <View style={{marginLeft: "auto"}}>
-          <Button 
-            title="View More"
-            color={"#b60c0cff"}
-            onPress={() => {
-              const updatePage: number = currentPage + 10
-              setCurrentPage(updatePage)
-          }}/>
-        </View>
-      </View>
-    </ScrollView>
-  );
+                { viewRegionPicker ? <View style={{backgroundColor: theme.background}}>
+                    <Text style={[styles.smallQuestion, {color: theme.text}]}>{"By flying to another region, you can see all the Pokémon in that region, and beyond."}</Text>
+                    <Picker
+                    style={{color: theme.text}}
+                    selectedValue={selectedRegion}
+                    onValueChange={(itemValue, _) => {
+                        setRegion(itemValue)
+                        setCurrentPage(0)
+                    }
+                    }>
+                    <Picker.Item label="Kanto" value="0" />
+                    <Picker.Item label="Johto" value="151" />
+                    <Picker.Item label="Hoenn" value="251" />
+                    <Picker.Item label="Sinnoh" value="386" />
+                    <Picker.Item label="Unova" value="493" />
+                    <Picker.Item label="Kalos" value="649" />
+                    <Picker.Item label="Alola" value="721" />
+                    <Picker.Item label="Galar" value="809" />
+                    <Picker.Item label="Paldea" value="905" />
+                </Picker>
+                </View> : null
+                }
+                <FlatList 
+                    data={listPokemon}
+                    numColumns={3}
+                    style={{paddingHorizontal: 10, backgroundColor: theme.background}}
+                    contentContainerStyle={{ gap: 10, margin: 10 }}
+                    columnWrapperStyle={{ gap: 10, justifyContent: "center", width: "100%" }}
+                    onEndReached={loadMorePokemon}
+                    onEndReachedThreshold={0.2}
+                    renderItem={({item}) => 
+                    <Link
+                        key={item.name}
+                        href={{ pathname: "/statistics", params: {name: item.name}}}
+                        style={[styles.cardLayout, 
+                            {
+                            // + 70 makes the opacity 70%
+                            // ignore type warning
+                            // @ts-ignore
+                            backgroundColor: bgColourByType[item.types[0].type.name] + 70,
+                        }]}>
+                        {/* for each pokemon, create a View with the key of pokemon.name, containing it's name ... */}
+                        <View style={styles.cardContent}>
+                
+                            {/* Name */}
+                            <Text key={item.name} style={[styles.name, {color: theme.title}]}>
+                            {item.name.toUpperCase()}
+                            </Text>
+                
+                            {/* Types */}
+                            <View style={styles.typesRow}>
+                            {item.types.map((type) => (
+                            <Text key={item.name + type.type.name} style={[styles.type, {color: theme.subtext}]}>{type.type.name}</Text>
+                            ))}
+                            </View>
+                
+                            {/* Sprites */}
+                            <View style={styles.imagesRow}>
+                            <Image
+                            source={{uri: item.imageFrontLink}}
+                            style={{ width: 150, height: 130 }} />
+                            </View>
+                        </View>
+                        </Link>
+                        }
+                        keyExtractor={item => item.name}
+                />
+            </SafeAreaView>
+        </SafeAreaProvider>
+      );
 }
 
 const styles = StyleSheet.create({
-  cardLayout: {
-    padding: 25,
-    borderRadius: 20,
-  },
+    question: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
 
-  cardContent: {
-    width: "100%", // needed to allow centring to take up full width and actually be centred
-    alignItems: "center", // center content horizontally and vertically inside each card
-  },
+    questionRow: {
+        flexDirection: "row",
+        width: "100%",
+        gap: 5,
+        justifyContent: "center",
+        alignItems: "center"
+    },
 
-  name: {
-    fontSize: 28,
-    fontWeight: 'bold'
-  },
+    smallQuestion: {
+        fontSize: 15,
+        fontStyle: "italic",
+        textAlign: "center",
+        paddingHorizontal: 5
+    },
 
-  type: {
-    fontSize: 20,
-    fontStyle: 'italic',
-    color: 'grey',
-  },
+    cardLayout: {
+        width: "33%",
+        padding: 8,
+        borderRadius: 10,
+    },
 
-  typesRow: {
-    flexDirection: "row",
-    justifyContent: "center",
-    gap: 10
-  },
+    cardContent: {
+        width: "100%", // needed to allow centring to take up full width and actually be centred
+        alignItems: "center", // center content horizontally and vertically inside each card
+    },
 
-  imagesRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
+    name: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    },
 
-  buttonRow: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-  }
+    type: {
+        fontSize: 14,
+        fontStyle: 'italic',
+    },
+
+    typesRow: {
+        flexDirection: "row",
+        justifyContent: "center",
+        gap: 10
+    },
+
+    imagesRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+
+    buttonRow: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    }
 })
