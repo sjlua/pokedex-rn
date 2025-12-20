c869a4711eace267a4f679c6883f0015872fc62f
Sean Lua <seanjlua10@gmail.com>
2025-12-21 00:30:07 +1100

    Added a light and dark colour palette

---
+++ constants/colours.js
@@ -0,0 +1,26 @@
+export const Colours = {
+    primary: "#b60c0cff",
+    warning: "#ff2020ff",
+
+    dark: {
+        title: "#fff",
+        subtext: "#d3d3d3ff",
+        text: "#eaeaeaff",
+        background: "#17161bff",
+        navBackground: "#201e2b",
+        iconColor: "#cececeff",
+        iconColorFocused: "#a2d8ffff",
+        uiBackground: "#17161bff",
+    },
+
+    light: { 
+        text: "#393939ff", 
+        subtext: "#d3d3d3ff",
+        title: "#201e2b", 
+        background: "#e0dfe8", 
+        navBackground: "#eseef",
+        iconColor: "#686477",
+        iconColorFocused: "#b60c0cff",
+        uiBackground: "#d6d5e1",
+    }
+}
