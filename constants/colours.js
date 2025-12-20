f9a270a2c760bcc7df7660dbffdc1ec39dd67846
Sean Lua <seanjlua10@gmail.com>
2025-12-21 01:15:21 +1100

    Better colour palette

--- constants/colours.js
+++ constants/colours.js
@@ -6,21 +6,21 @@ export const Colours = {
         title: "#fff",
         subtext: "#d3d3d3ff",
         text: "#eaeaeaff",
-        background: "#17161bff",
-        navBackground: "#201e2b",
+        background: "#16181bff",
+        navBackground: "#1e252bff",
         iconColor: "#cececeff",
-        iconColorFocused: "#a2d8ffff",
+        iconColorFocused: "#a2d8ffff", // also used for buttons
         uiBackground: "#17161bff",
     },
 
     light: { 
-        text: "#393939ff", 
-        subtext: "#d3d3d3ff",
+        text: "#2b2b2bff", 
+        subtext: "#686868ff",
         title: "#201e2b", 
-        background: "#e0dfe8", 
-        navBackground: "#eseef",
-        iconColor: "#686477",
-        iconColorFocused: "#b60c0cff",
+        background: "#f0f0f0ff", 
+        navBackground: "#fff",
+        iconColor: "#455966ff",
+        iconColorFocused: "#0a93d8ff",
         uiBackground: "#d6d5e1",
     }
 }
