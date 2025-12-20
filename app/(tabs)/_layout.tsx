7bb9deea1c3f99e96b61a8d166cc239dbbdd6cf5
Sean Lua <seanjlua10@gmail.com>
2025-12-21 00:30:50 +1100

    Added light/dark theming support and re-arranged some of the routes

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,39 +1,38 @@
 import { MaterialCommunityIcons, MaterialIcons } from "@expo/vector-icons";
 import { BlurView } from "expo-blur";
 import { Tabs } from "expo-router";
-import { StyleSheet } from "react-native";
+import { StyleSheet, useColorScheme } from "react-native";
+import { Colours } from "../../constants/colours";
 
 export default function TabsLayout() {
+  // Get the colour scheme from app.json userInterfaceStyle, null fallback is light
+  const colourScheme = useColorScheme() ?? 'light'
+  const theme = Colours[colourScheme]
+
   return (
     <Tabs screenOptions={{ 
-      tabBarActiveTintColor: "#b60c0cff", 
-      tabBarStyle: { position: 'absolute' }, 
+      headerStyle: { backgroundColor: theme.navBackground },
+      headerTintColor: theme.title,
+      tabBarActiveTintColor: theme.iconColorFocused,
+      tabBarInactiveTintColor: theme.iconColor,
+      tabBarStyle: { position: 'absolute' },
 
       tabBarBackground: () => (
         // see https://docs.expo.dev/versions/latest/sdk/blur-view/#experimentalblurmethod
         // on why experimentalBlurMethod="dimezisBlurView" is used
         <BlurView tint="prominent" intensity={80} style={StyleSheet.absoluteFill} experimentalBlurMethod="dimezisBlurView" />
       ),
-      headerBackground: () => (
-          <BlurView tint="prominent" intensity={80} style={StyleSheet.absoluteFill} experimentalBlurMethod="dimezisBlurView" />
-        )
+      // EXPERIMENTAL
+      // headerBackground: () => (
+      //     <BlurView tint="prominent" intensity={80} style={StyleSheet.absoluteFill} experimentalBlurMethod="dimezisBlurView" />
+      //   )
       }}>
       <Tabs.Screen
         name="index"
         options={{
-          title: "Pokédex RN",
+          title: "Dex",
           tabBarIcon: ({ color, size }) => (
             <MaterialCommunityIcons name="pokeball" size={size} color={color} />
-          )
-        }}
-      />
-
-      <Tabs.Screen
-        name="collect"
-        options={{
-          title: "Collection",
-          tabBarIcon: ({ color, size }) => (
-            <MaterialIcons name="my-library-add" size={size} color={color} />
           )
         }}
       />
