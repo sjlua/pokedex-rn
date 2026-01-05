97500e4072ee087d81ba183c269bd63409694ef3
Sean Lua <seanjlua10@gmail.com>
2026-01-05 22:32:07 +1100

    Added new colour for buttons

--- constants/colours.js
+++ constants/colours.js
@@ -1,26 +1,28 @@
 export const Colours = {
-    primary: "#b60c0cff",
-    warning: "#ff2020ff",
+  primary: "#b60c0cff",
+  warning: "#ff2020ff",
 
-    dark: {
-        title: "#fff",
-        subtext: "#d3d3d3ff",
-        text: "#eaeaeaff",
-        background: "#16181bff",
-        navBackground: "#1e252bff",
-        iconColor: "#cececeff",
-        iconColorFocused: "#a2d8ffff", // also used for buttons
-        uiBackground: "#17161bff",
-    },
+  dark: {
+    title: "#fff",
+    subtext: "#d3d3d3ff",
+    text: "#eaeaeaff",
+    background: "#16181bff",
+    navBackground: "#1e252bff",
+    buttonBackground: "#0C5085", // works with standard white text
+    iconColor: "#cececeff",
+    iconColorFocused: "#a2d8ffff",
+    uiBackground: "#17161bff",
+  },
 
-    light: { 
-        text: "#2b2b2bff", 
-        subtext: "#686868ff",
-        title: "#201e2b", 
-        background: "#f0f0f0ff", 
-        navBackground: "#fff",
-        iconColor: "#455966ff",
-        iconColorFocused: "#0a93d8ff",
-        uiBackground: "#d6d5e1",
-    }
-}
+  light: {
+    text: "#2b2b2bff",
+    subtext: "#686868ff",
+    title: "#201e2b",
+    buttonBackground: "#70C0FF", // works with standard white text
+    background: "#f0f0f0ff",
+    navBackground: "#fff",
+    iconColor: "#455966ff",
+    iconColorFocused: "#0a93d8ff",
+    uiBackground: "#d6d5e1",
+  },
+};
